beac9165e03a7221b87ee8f8544776a5937476f9
王仁欢 <wrh4285@163.com>
2015-10-30 18:22:29 +0800

    2015-10-30 微信回复规则支持单图文

--- control/wechat_rule.php
+++ control/wechat_rule.php
@@ -12,17 +12,46 @@ $template = 'wechat_rule/';
 assign('subTitle', '微信回复规则管理');
 
 $action = 'view|add|edit|delete';
-$operation = 'add|edit|remove';
+$operation = 'add|edit|remove|get_content';
 
 $act = check_action($action, getGET('act'));
 $act = ( $act == '' ) ? 'view' : $act;
 
+$msgType_array = array(
+    'text' => '文本',
+    'news' => '图文',
+);
+assign('msgType', $msgType_array);
+
 $opera = check_action($operation, getPOST('opera'));
 
-if('edit' == $opera)
-{
+if( 'get_content' == $opera ) {
     $response = array('error'=>1, 'msg'=>'', 'errmsg'=>array());
 
+    $id = intval(getPOST('id'));
+    if( 0 >= $id ) {
+        $response['msg'] = '参数错误';
+        echo json_encode($response);
+        exit;
+    }
+
+    $get_content = 'select `title`, `description`, `original` from '.$db->table('content').' where id = \''.$id.'\' limit 1';
+    $content = $db->fetchRow($get_content);
+    if( $content ) {
+        $content['time'] = 'xxxx年xx月';
+        $content['thumb'] = $content['original'];
+        $content['url'] = '/content.php?id='.$content['id'];
+        $response['content'] = $content;
+        $response['error'] = 0;
+    } else {
+        $response['msg'] = $get_content;
+    }
+    echo json_encode($response);
+    exit;
+}
+
+if('edit' == $opera) {
+    $response = array('error'=>1, 'msg'=>'', 'errmsg'=>array());
     $eid = intval(getPOST('eid'));
     $rule = trim(getPOST('rule'));
     $response_content = trim(getPOST('response_content'));
@@ -30,6 +59,9 @@ if('edit' == $opera)
     $order_view = intval(getPOST('order_view'));
     $enabled = intval(getPOST('enabled'));
     $match_mode = intval(getPOST('match_mode'));
+
+    $msgType = trim(getPOST('msgType'));
+    $content_id = intval(getPOST('content_id'));
 
     if($eid == '')
     {
@@ -43,11 +75,31 @@ if('edit' == $opera)
         $rule = $db->escape($rule);
     }
 
-    if($response_content == '')
-    {
-        $response['errmsg']['rule'] = '-请填写回复内容';
+    if( $msgType == '' ) {
+        $response['errmsg']['msgType'] = '-请选择回复类型';
+    } else if( !array_key_exists($msgType, $msgType_array ) ) {
+        $response['errmsg']['msgType'] = '-请选择回复类型';
     } else {
-        $response_content = $db->escape($response_content);
+        $msgType = $db->escape($msgType);
+        if( $msgType == 'news' ) {
+            if( $content_id <= 0 ) {
+                $content_id = 0;
+            }
+            if( $content_id != 0 ) {
+                $check_content = 'select * from '.$db->table('content').' where id = \''.$content_id.'\' limit 1';
+                $content = $db->fetchRow($check_content);
+                if( empty($content) ) {
+                    $content_id = 0;
+                }
+            }
+        }
+    }
+    if( $msgType == 'text' ) {
+        if ($response_content == '') {
+            $response['errmsg']['rule'] = '-请填写回复内容';
+        } else {
+            $response_content = $db->escape($response_content);
+        }
     }
 
     if($name == '')
@@ -65,15 +117,36 @@ if('edit' == $opera)
     if(count($response['errmsg']) == 0 && $response['msg'] == '')
     {
         $db->begin();
+        $no_need = false;
+        if( $msgType == 'text' ) {
+            $response_data = array(
+                'content' => $response_content,
+                'msgType' => 'text',
+                'title' => '',
+                'description' => '',
+                'picUrl' => '',
+                'url' => '',
+            );
+        } elseif( $msgType == 'news' ) {
+            if( $content_id != 0 ) {
+                $response_data = array(
+                    'msgType' => 'news',
+                    'content' => $content['wap_content'],
+                    'title' => $content['title'],
+                    'description' => $content['description'],
+                    'picUrl' => 'http://'.$_SERVER['HTTP_HOST'].'/'.$content['original'],
+                    'url' => 'http://'.$_SERVER['HTTP_HOST'].'/article.php?id='.$content['id'],
+                );
+            } else {
+                $no_need = true;
+            }
 
-        $response_data = array(
-            'content' => $response_content
-        );
+        }
 
         $get_response_id = 'select `response_id` from '.$db->table('wx_rule').' where `id`='.$eid;
         $response_id = $db->fetchOne($get_response_id);
 
-        if($db->autoUpdate('wx_response', $response_data, '`id`='.$response_id))
+        if( $no_need || $db->autoUpdate('wx_response', $response_data, '`id`='.$response_id))
         {
             $rule_data = array(
                 'match_mode' => $match_mode,
@@ -113,6 +186,9 @@ if('add' == $opera)
     $enabled = intval(getPOST('enabled'));
     $match_mode = intval(getPOST('match_mode'));
 
+    $msgType = trim(getPOST('msgType'));
+    $content_id = intval(getPOST('content_id'));
+
     if($rule == '')
     {
         $response['errmsg']['rule'] = '-请填写关键词';
@@ -120,11 +196,30 @@ if('add' == $opera)
         $rule = $db->escape($rule);
     }
 
-    if($response_content == '')
-    {
-        $response['errmsg']['rule'] = '-请填写回复内容';
+    if( $msgType == '' ) {
+        $response['errmsg']['msgType'] = '-请选择回复类型';
+    } else if( !array_key_exists($msgType, $msgType_array ) ) {
+        $response['errmsg']['msgType'] = '-请选择回复类型';
     } else {
-        $response_content = $db->escape($response_content);
+        $msgType = $db->escape($msgType);
+        if( $msgType == 'news' ) {
+            if( $content_id <= 0 ) {
+                $response['errmsg']['content_id'] = '-请选择资讯';
+            } else {
+                $check_content = 'select * from '.$db->table('content').' where id = \''.$content_id.'\' limit 1';
+                $content = $db->fetchRow($check_content);
+                if( empty($content) ) {
+                    $response['errmsg']['content_id'] = '-请选择资讯';
+                }
+            }
+        }
+    }
+    if( $msgType == 'text' ) {
+        if ($response_content == '') {
+            $response['errmsg']['rule'] = '-请填写回复内容';
+        } else {
+            $response_content = $db->escape($response_content);
+        }
     }
 
     if($name == '')
@@ -142,11 +237,21 @@ if('add' == $opera)
     if(count($response['errmsg']) == 0)
     {
         $db->begin();
-
-        $response_data = array(
-            'msgType'=>'text',
-            'content' => $response_content
-        );
+        if( $msgType == 'text' ) {
+            $response_data = array(
+                'msgType' => 'text',
+                'content' => $response_content
+            );
+        } elseif( $msgType == 'news' ) {
+            $response_data = array(
+                'msgType' => 'news',
+                'content' => $content['wap_content'],
+                'title' => $content['title'],
+                'description' => $content['description'],
+                'picUrl' => 'http://'.$_SERVER['HTTP_HOST'].'/'.$content['original'],
+                'url' => 'http://'.$_SERVER['HTTP_HOST'].'/article.php?id='.$content['id'],
+            );
+        }
 
         if($db->autoInsert('wx_response', array($response_data)))
         {
@@ -166,6 +271,7 @@ if('add' == $opera)
                 $db->commit();
                 $response['error'] = 0;
                 $response['msg'] = '新增规则成功';
+                $response['msgType'] = $msgType;
             } else {
                 $db->rollback();
                 $response['msg'] = '新增规则失败，请稍后再试';
@@ -189,10 +295,14 @@ if('edit' == $act)
         show_system_message('参数错误');
     }
 
-    $get_rule = 'select r.*,res.content from '.$db->table('wx_rule').' as r join '.$db->table('wx_response').' as res on r.response_id=res.id where r.`id`='.$id;
+    $get_rule = 'select r.*,res.content,res.msgType,res.picUrl,res.url,res.description,res.title from '.$db->table('wx_rule').' as r join '.$db->table('wx_response').' as res on r.response_id=res.id where r.`id`='.$id;
     $rule = $db->fetchRow($get_rule);
-
     assign('rule', $rule);
+
+    //获取资讯标题
+    $get_content_list = 'select `title`, `id` from '.$db->table('content').' where status = 1 order by id desc';
+    $content_list = $db->fetchAll($get_content_list);
+    assign('content_list', $content_list);
 }
 
 if('view' == $act)
@@ -232,6 +342,14 @@ if('view' == $act)
     $rule_list = $db->fetchAll($get_rule_list);
 
     assign('rule_list', $rule_list);
+}
+
+if( 'add' == $act ) {
+    //获取资讯
+    $get_content_list = 'select `title`, `id` from '.$db->table('content').' where status = 1 order by id desc';
+    $content_list = $db->fetchAll($get_content_list);
+    assign('content_list', $content_list);
+
 }
 
 $template .= $act.'.phtml';
